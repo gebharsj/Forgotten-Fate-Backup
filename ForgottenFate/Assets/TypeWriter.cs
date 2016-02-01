a4ceb6dcefa127944675d572a49fb07012c5aa6d
gebharsj <gebharsj@gmatc.matc.edu>
2016-02-01 15:32:15 -0600

    Added a new unity project

---
+++ ForgottenFate/Assets/TypeWriter.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TypeWriter : MonoBehaviour
+{
+	private const float WAIT_TIME = 0.05f;
+	
+	private float waitTimer = 0.0f;
+	public string wholeText = "";
+	private string typewriterText = "";
+	private int currentIndex = 0;
+
+	private bool clickActivation = false;
+
+
+	void Start(){
+		wholeText = "Still Pulling From TypeWriter";
+	}
+
+
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.Mouse0)) { 
+			clickActivation = true;
+		}
+
+		if (clickActivation)
+		{
+			//if (Input.GetMouseButton (LEFT_MOUSE_BUTTON))
+			waitTimer += Time.deltaTime;
+			wholeText = GameObject.Find ("Basic").GetComponent<TriggerWriting> ().wholeText;
+			
+			if (waitTimer > WAIT_TIME && currentIndex < wholeText.Length) {            
+				typewriterText += wholeText [currentIndex];
+				waitTimer = 0.0f;
+				++currentIndex;
+			}
+
+			if (currentIndex == wholeText.Length)
+			{
+				clickActivation = false;
+			}
+
+		}
+	}
+	
+	void OnGUI()
+	{
+		GUI.TextArea(new Rect(0.0f, 0.0f, 300.0f, 100.0f), typewriterText);
+	}
+}
