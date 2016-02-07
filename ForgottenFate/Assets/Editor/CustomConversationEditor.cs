ef823432af611fee414aab1a3f0500b72f7055ab
Sam <gebharsj@gmatc.matc.edu>
2016-02-07 14:38:01 -0600

    Added the text box, and started a custom editor script.
    By start, I mean created the file and sort of wrote some stuff that
    tutorials had.

---
+++ ForgottenFate/Assets/Editor/CustomConversationEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(ConversationScript))]
+public class CustomConversationEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        string text;
+        DrawDefaultInspector();
+        //base.OnInspectorGUI();
+
+        var cs = (ConversationScript)target;
+        cs.conversation = EditorGUILayout.TextArea(text, GUILayout.Height(20));
+    }
+}
---
+++ ForgottenFate/Assets/Text/ConversationScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConversationScript : MonoBehaviour {
+
+    public string[] conversation;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
