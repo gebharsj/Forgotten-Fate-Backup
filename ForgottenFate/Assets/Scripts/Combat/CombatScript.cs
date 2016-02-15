e761b7bd7cd629c6df6481effe5e11554c63b1d6
PeterHoppers <hoppepj@gmatc.matc.edu>
2016-02-15 16:11:21 -0600

    Dash Attack and Sprint

--- ForgottenFate/Assets/Scripts/Combat/CombatScript.cs
+++ ForgottenFate/Assets/Scripts/Combat/CombatScript.cs
@@ -11,6 +11,8 @@ public class CombatScript : MonoBehaviour
 	public float maxMana;
 	public float manaRecovery = 0.03f;
 	private float mana;
+	public float normalDamage = 3;
+	[HideInInspector]
 	public float playerDamage = 3;
 	public float attackSpeed = 5.0f;
 	public int defense;
@@ -21,24 +23,27 @@ public class CombatScript : MonoBehaviour
 	public int maxHealth = 65;
 	float health;
 	public float criticalChance = 0.03f;
+	private float chargeMultiplier = 10.0f;
+	[HideInInspector]
+	public float chargeDistance;
 	
-
+	
 	[HideInInspector]
 	public int splash;
 	[HideInInspector]
 	public float attackRate;  //rate of attack
-
+	
 	void Awake()
 	{
 		mana = maxMana;
 		health = maxHealth;
 	}
-
+	
 	// Use this for initialization
 	void Start () 
 	{
-
-
+		
+		
 	}
 	
 	// Update is called once per frame
@@ -47,12 +52,12 @@ public class CombatScript : MonoBehaviour
 		if (health == 0)
 		{
 			Destroy (gameObject);
-			//play animation
+			//pay animation
 			//play sound
 		}
-
-
-
+		
+		
+		
 		//setting the scale of objects to range of melee weapon
 		up.transform.localScale = new Vector3(0, meleeRange, 0);
 		up.transform.localPosition = new Vector3(0, meleeAdjustment, 0);
@@ -62,59 +67,83 @@ public class CombatScript : MonoBehaviour
 		left.transform.localPosition = new Vector3(-meleeAdjustment, 0, 0);
 		right.transform.localScale = new Vector3(meleeRange, 0, 0);
 		right.transform.localPosition = new Vector3(meleeAdjustment, 0, 0);
-
-		if (criticalChance > 0.3f)
+		
+		if (criticalChance > 0.08f)
 			criticalChance = 0.08f;
-
-
+		
+		
 		if(Input.GetMouseButtonDown(0) && attackRate == 0)  //left click
 		{
-			attackRate = 5;
-			splash = 1; 
-			//the value of this variable determines how many foes can be hit in a single attack
-			//since it's currently set to 1, only one foe can be hit at a time.
-			//certain spells, such as a multi attack, will require this variable to increase.
-
+			if (!self.GetComponent<PlayerMovement>().isSprinting)
+			{
+				playerDamage = normalDamage;
+				splash = 1; 
+				attackRate = 5;
+				
+				//the value of this variable determines how many foes can be hit in a single attack
+				//since it's currently set to 1, only one foe can be hit at a time.
+				//certain spells, such as a multi attack, will require this variable to increase.
+				
 				//play sound
-			//attack animation
+				//attack animation
+			}
+			if (self.GetComponent<PlayerMovement>().isSprinting)
+			{
+				playerDamage = normalDamage * chargeMultiplier;
+				splash = 5; 
+				chargeDistance = 1.2f;
+				self.GetComponent<PlayerMovement>().moveX = self.GetComponent<PlayerMovement>().moveX * 25;
+				self.GetComponent<PlayerMovement>().moveY = self.GetComponent<PlayerMovement>().moveY * 25;
+				
+			}
 		}
-
-			//attack speed
-			if (attackRate > 0)
-		    {
+		
+		//attack speed
+		if (attackRate > 0)
+		{
 			attackRate -= attackSpeed * Time.deltaTime;
 			//prevents moving during attack
 			self.GetComponent<PlayerMovement>().moveX = 0;
 			self.GetComponent<PlayerMovement>().moveY = 0;
-		    }
-
-			if(attackRate < 0)
-				attackRate = 0;
-			if(attackSpeed > 50)
-				attackSpeed = 50;
-
+		}
+		
+		if(attackRate < 0)
+			attackRate = 0;
+		if(attackSpeed > 50)
+			attackSpeed = 50;
+		
 		//mana recovery
 		if (mana < maxMana) 
 			mana += manaRecovery * Time.deltaTime;
-
+		
 		if (defense < 1)
 			defense = 1;
 		
-
-
+		//charge recovery
+		if (chargeDistance > 1) 
+			chargeDistance -= chargeDistance * Time.deltaTime;
+		
+		if (chargeDistance > 0 && chargeDistance <= 1) 
+		{
+			attackRate = 10;
+			chargeDistance = 0;
+		}
+		
+		
+		
 		//casting magic
 		if(Input.GetMouseButtonDown(1))  //right click
 		{
-
+			
 		}
-
+		
 		//facing right
 		if (self.GetComponent<PlayerMovement>().moveX > 0)
 		{
-		up.SetActive (false);
-		down.SetActive (false);
-		left.SetActive (false);
-		right.SetActive (true);
+			up.SetActive (false);
+			down.SetActive (false);
+			left.SetActive (false);
+			right.SetActive (true);
 		}
 		//facing left
 		if (self.GetComponent<PlayerMovement>().moveX < 0)
@@ -146,3 +175,4 @@ public class CombatScript : MonoBehaviour
 		yield return new WaitForSeconds(0.5f);
 	}
 }
+
--- ForgottenFate/Assets/Scripts/Combat/EnemiesReceiveDamage.cs
+++ ForgottenFate/Assets/Scripts/Combat/EnemiesReceiveDamage.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class EnemiesReceiveDamage : MonoBehaviour {
-	public int enemyLvl;
 	public float maxHp;
 	float hp;
 	private bool hit = false;
@@ -22,138 +21,125 @@ public class EnemiesReceiveDamage : MonoBehaviour {
 	public Color32 endColor;
 	float hitChance;
 	public float criticalChance =0.03f;
-
-	//----exp-----
-	private float exp;
-	private int playerLvl = 1;
-	private float maxExp;
-
-
+	public Rigidbody rb;
+	
+	
 	void Awake()
 	{
 		hp = maxHp;
 	}
-
-
+	
+	
 	// Use this for initialization
 	void Start () 
 	{
-
+		
+		rb = GetComponent<Rigidbody>();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-
+		
 		if (hp <= 0) 
 		{
 			Destroy (gameObject);
-
-			exp += (enemyLvl * 10);
-
-			maxExp = 50 * (1) * playerLvl;
-
-			Debug.Log(maxExp + "MaxEXP");
-
-			Debug.Log(exp + " Current Exp");
-
-			if (exp > maxExp)
-			{
-				playerLvl ++;
-				//InitCBT("LEVEL UP").GetComponent<Animator>().SetTrigger("Miss");
-				exp = 0;
-				_player.GetComponent<CombatScript> ().playerDamage ++;
-				Debug.Log(playerLvl + "Player LEVEL");
-				Debug.Log(_player.GetComponent<CombatScript> ().playerDamage + "Damage");
-			}
-
 		}
-
+		
 		if (defense < 1)
 			defense = 1;
 		
-	}
-
-	void OnCollisionStay(Collision col)
-	{
-		//dealing damage to object
-		if (col.gameObject.tag != "PlayerMelee" && hit == false && _player.GetComponent<CombatScript> ().attackRate > 0)
+		if (_player.GetComponent<CombatScript> ().chargeDistance == 0)
 		{
-			//using calculations to determine the chance of landing a hit.
-			//this also makes sure that chance of hitting cannot be greater or less than a set amount.
-			hitChance = Random.Range(0.0f,1.0f);
-			defDex_calc = (_player.GetComponent<CombatScript> ().dexterity - defense)/_player.GetComponent<CombatScript> ().dexterity;
-			if (defDex_calc > 0.95f)
-			defDex_calc = .95f;
-			if (defDex_calc < 0.05f)
-			defDex_calc = .05f;
-			_player.GetComponent<CombatScript> ().splash -= 1;
-
-			Debug.Log (defDex_calc);
-
-			//causing damage and estimating chances
-
-			if (hitChance <=1 && (hitChance >= defDex_calc))
-			{
-				damageTaken = 0;
-				InitCBT("*miss*").GetComponent<Animator>().SetTrigger("Miss");
-				hitChance =2;
-				hit = true;
-			}
-			if (hitChance <=1 && (hitChance < defDex_calc))
-			{
-				hitChance =2;
-				hit = true;
-				damageTaken = _player.GetComponent<CombatScript> ().playerDamage;
-
-				if (damageTaken > armor + 1)
-					damageTaken = damageTaken - armor;
-				else
-					damageTaken = 2;
-
-				damageTaken = Random.Range (damageTaken*0.7f, damageTaken);
-				damageTaken = -damageTaken;
-				criticalHit = Random.Range (0, 1.0f);
-				//print ("crit" + criticalHit);
-
-				//Damage caused by critical hit (critical hits do 5 times more than normal damage).
-				if (criticalHit < 2 && criticalHit <= _player.GetComponent<CombatScript> ().criticalChance)
-					{
+			rb.drag = 10000;
+			rb.mass = 50;
+		}
+		
+	}
+	
+	void OnTriggerStay(Collider col)
+	{
+		
+		if (_player.GetComponent<CombatScript> ().splash > 0) 
+		{
+			//dealing damage to object
+			if (col.gameObject.tag == "PlayerMelee" && hit == false && _player.GetComponent<CombatScript> ().attackRate > 0) {
+				//using calculations to determine the chance of landing a hit.
+				//this also makes sure that chance of hitting cannot be greater or less than a set amount.
+				hitChance = Random.Range (0.0f, 1.0f);
+				defDex_calc = (_player.GetComponent<CombatScript> ().dexterity - defense) / _player.GetComponent<CombatScript> ().dexterity;
+				if (defDex_calc > 0.95f)
+					defDex_calc = .95f;
+				if (defDex_calc < 0.05f)
+					defDex_calc = .05f;
+				_player.GetComponent<CombatScript> ().splash -= 1;
+				
+				Debug.Log (defDex_calc);
+				
+				//causing damage and estimating chances
+				
+				if (hitChance <= 1 && (hitChance >= defDex_calc)) {
+					damageTaken = 0;
+					InitCBT ("*miss").GetComponent<Animator> ().SetTrigger ("Miss");
+					hitChance = 2;
+					hit = true;
+				}
+				if (hitChance <= 1 && (hitChance < defDex_calc)) {
+					hitChance = 2;
+					hit = true;
+					damageTaken = _player.GetComponent<CombatScript> ().playerDamage;
+					if (damageTaken > armor + 1)
+						damageTaken = damageTaken - armor;
+					else
+						damageTaken = 2;
+					damageTaken = Random.Range (damageTaken * 0.7f, damageTaken);
+					damageTaken = -damageTaken;
+					criticalHit = Random.Range (0, 1.0f);
+					//print ("crit" + criticalHit);
+					
+					//Damage caused by critical hit (critical hits do 5 times more than normal damage).
+					if (criticalHit < 2 && criticalHit <= _player.GetComponent<CombatScript> ().criticalChance) {
 						damageTaken = (damageTaken * 5);
-						damageTaken = Mathf.Round(damageTaken * 10f) / 10f;
-						InitCBT(damageTaken.ToString()).GetComponent<Animator>().SetTrigger("Crit");
+						damageTaken = Mathf.Round (damageTaken * 10f) / 10f;
+						InitCBT (damageTaken.ToString ()).GetComponent<Animator> ().SetTrigger ("Crit");
 						//print ("damageTaken " + damageTaken);
 						hp += damageTaken;
-						calculator = hp/maxHp;
-						SetHealth(calculator);
+						calculator = hp / maxHp;
+						SetHealth (calculator);
 						criticalHit = 2;
 						//sound
 					}
 					//Nomal damage
-				if (criticalHit < 2 && criticalHit != _player.GetComponent<CombatScript> ().criticalChance)
-					{
-						damageTaken = Mathf.Round(damageTaken * 10f) / 10f;
+					if (criticalHit < 2 && criticalHit != _player.GetComponent<CombatScript> ().criticalChance) {
+						damageTaken = Mathf.Round (damageTaken * 10f) / 10f;
 						//print ("damageTaken " + damageTaken);
-						InitCBT(damageTaken.ToString()).GetComponent<Animator>().SetTrigger("Hit");
+						InitCBT (damageTaken.ToString ()).GetComponent<Animator> ().SetTrigger ("Hit");
 						hp += damageTaken;
-						calculator = hp/maxHp;
-						SetHealth(calculator);
+						calculator = hp / maxHp;
+						SetHealth (calculator);
 						criticalHit = 2;
 						//sound
 					}
 				}
-
+				
+			}
+			if (_player.GetComponent<CombatScript> ().chargeDistance > 0)
+			{
+				rb.drag = 0;
+				rb.mass = 1;
+			}
 		}
 		//resetting the conditions for damage
 		if (hit == true && _player.GetComponent<CombatScript> ().attackRate == 0) 
 		{
 			hit = false;
 		}
-
+		
 	}
-
-
-
+	
+	
+	
 	//this method or function calls for the "CBT" prefab's transforms and animation seuquences.
 	// this is used for animating the damage text shown when the player hits a target.
 	GameObject InitCBT(string text)
@@ -164,22 +150,22 @@ public class EnemiesReceiveDamage : MonoBehaviour {
 		tempRect.transform.localPosition = CBTPrefab.transform.localPosition;
 		tempRect.transform.localScale = CBTPrefab.transform.localScale;
 		tempRect.transform.localRotation = CBTPrefab.transform.localRotation;
-
+		
 		Debug.Log (tempRect.transform.localPosition);
-
+		
 		temp.GetComponent<Text>().text = text;
 		Destroy(temp.gameObject, 3);
 		//temp.GetComponent<Animator>().SetTrigger("Hit");
 		return temp;
 	}
-
+	
 	public void SetHealth (float myHealth)
 	{
 		//"myHealth" needs to be set between the values of 0 and 1: 1 being 100%.
 		healthBar.transform.localScale = new Vector3 (myHealth, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
 		healthBar.color = Color.Lerp(endColor, startColor, calculator);
 	}
-
-
-
+	
+	
+	
 }
