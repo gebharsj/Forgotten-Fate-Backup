3d8994357524f650ca9ec43adcdabc0abc9454ac
gebharsj <gebharsj@gmatc.matc.edu>
2016-02-16 16:23:08 -0600

    Updated Build

---
+++ ForgottenFateMaster/Assets/PauseMenu/Resume.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resume : MonoBehaviour {
+
+	public GameObject pauseMenu;
+	
+	public void ResumeGame()
+	{
+		pauseMenu.SetActive (false);
+		Screen.lockCursor = true;
+		Time.timeScale = 1;
+	}
+}
