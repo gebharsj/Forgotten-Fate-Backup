18bef56770c65e7da301f740fe202d683d545db5
PeterHoppers <hoppepj@gmatc.matc.edu>
2016-04-03 22:13:37 -0500

    Super Rough Story Outline
    -Added More Quest Scripts
    -Added New "Art"
    -Added More Dialogue
    
    Located Within Peter's Stuff, Sam's Stuff, have fun!

--- Peter's Stuff/Sam's Stuff/ForgottenFateMaster/Assets/Scripts/Quest/CompletedQuest.cs
+++ Peter's Stuff/Sam's Stuff/ForgottenFateMaster/Assets/Scripts/Quest/CompletedQuest.cs
@@ -7,10 +7,18 @@ public class CompletedQuest : MonoBehaviour {
 	public int NPCFinishedIndex;
 	public GameObject[] NPCDialogue;
 	public int[] targetIndexCompleted;
+	public GameObject questItem;
+	public Transform questItemLocation;
+	public GameObject moveNPC;
+	public Transform locationNPC;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		if (questItem != null) 
+		{
+			questItem.transform.position = new Vector2 (-1000, -1000);
+		}
 	}
 	
 	// Update is called once per frame
@@ -22,6 +30,16 @@ public class CompletedQuest : MonoBehaviour {
 			{
 				NPCDialogue[i].GetComponent<TriggerText>().index = targetIndexCompleted[i];
 			}
+
+			if (questItem != null)
+			{
+				questItem.transform.position = questItemLocation.position;
+			}
+			if (moveNPC != null)
+			{
+				moveNPC.transform.position = locationNPC.position;
+			}
+			this.gameObject.SetActive(false);
 		}
 	
 	}
