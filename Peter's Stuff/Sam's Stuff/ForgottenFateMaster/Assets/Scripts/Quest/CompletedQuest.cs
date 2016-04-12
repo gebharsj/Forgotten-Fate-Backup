02276d901df02c2dd46ecbc566a0cc8baae70e18
PeterHoppers <hoppepj@gmatc.matc.edu>
2016-04-12 14:35:49 -0500

    Items Move After Talking

--- Peter's Stuff/Sam's Stuff/ForgottenFateMaster/Assets/Scripts/Quest/CompletedQuest.cs
+++ Peter's Stuff/Sam's Stuff/ForgottenFateMaster/Assets/Scripts/Quest/CompletedQuest.cs
@@ -7,18 +7,13 @@ public class CompletedQuest : MonoBehaviour {
 	public int NPCFinishedIndex;
 	public GameObject[] NPCDialogue;
 	public int[] targetIndexCompleted;
-	public GameObject questItem;
-	public Transform questItemLocation;
 	public GameObject moveNPC;
 	public Transform locationNPC;
 
 	// Use this for initialization
 	void Start () 
 	{
-		if (questItem != null) 
-		{
-			questItem.transform.position = new Vector2 (-1000, -1000);
-		}
+
 	}
 	
 	// Update is called once per frame
@@ -31,10 +26,6 @@ public class CompletedQuest : MonoBehaviour {
 				NPCDialogue[i].GetComponent<TriggerText>().index = targetIndexCompleted[i];
 			}
 
-			if (questItem != null)
-			{
-				questItem.transform.position = questItemLocation.position;
-			}
 			if (moveNPC != null)
 			{
 				moveNPC.transform.position = locationNPC.position;
